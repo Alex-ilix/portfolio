317bfb31a826210e6ed5ac460fc0753d02da24a4
Alexandre SIMONGIOVANNI <alexandre.simongiovanni@etu.umotpellier.fr>
2024-12-24 15:30:03 +0100

    8

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 // Optional: Handle additional interactivity (e.g., mobile responsiveness, animations).
 console.log("Script chargé avec succès.");
+const galaxy = document.querySelector('.galaxy');
+
